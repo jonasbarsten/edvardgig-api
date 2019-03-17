e57360967506ef635e43c4b8fa325f790e830d9f
Jonas Barsten <jonasbarsten@gmail.com>
2019-03-17 20:15:46 +0100

    Checking if user already have the license they are trying to buy

--- services/billing/billing.js
+++ services/billing/billing.js
@@ -3,22 +3,35 @@ import { calculateCost } from "../../libs/billing-lib";
 import { success, failure } from "../../libs/response-lib";
 
 import createLicense from '../licenses/create';
+import { listLicenses } from '../licenses/list';
 
 export async function main(event, context) {
-  // const { storage, source } = JSON.parse(event.body);
-  // const amount = calculateCost(storage);
-  // const description = "Scratch charge";
-
+  
   const { product, email, source } = JSON.parse(event.body);
   const amount = calculateCost(product);
   const userId = event.requestContext.identity.cognitoIdentityId;
   const description = `Charge for ${product}`;
+  const usersLicenses = await listLicenses(userId);
+  const usersLicensesJSON = JSON.parse(usersLicenses.body);
 
-  const license = await createLicense(userId, product);
+  let licenseExists = false;
+
+  for (var i = 0; i < usersLicensesJSON.length; i++) {
+    if (usersLicensesJSON[i].product === product) {
+      licenseExists = true;
+      break;
+    }
+  }
+
+  if (licenseExists) {
+    return failure({ message: 'You already have this license' });
+  }
 
   if (amount === 0) {
     return failure({ message: 'No matching products' });
   }
+
+  const license = await createLicense(userId, product);
 
   if (license.statusCode !== 200) {
     return failure({ message: 'Could not create license' });
@@ -35,7 +48,7 @@ export async function main(event, context) {
       receipt_email: email,
       statement_descriptor: `Edvard Gig - ${product}`,
       currency: "usd",
-      metadata: { email, userId, licenseId: license.licenseId }
+      metadata: { email, license: license.body }
     });
     return success({ status: true });
   } catch (e) {
